43c02e7ccf009fe3ae1d2a98714e0228e0f3dce1
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2023-10-14 07:22:27 +0800

    update
    eto latest

---
+++ DAO/ConnectionDb.vb
@@ -0,0 +1,9 @@
+﻿Public Class ConnectionDb
+
+    Private DIRECTORY = My.Application.Info.DirectoryPath
+    Private Const CONN As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=D:\C# projects\TejeroProject3\JDTProject3\Caribean Adventures.mdb"
+
+    Sub New()
+
+    End Sub
+End Class
