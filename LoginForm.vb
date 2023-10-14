5b0c62feffd2ae98473873306210c5a0ccd320f4
COMP17 <COMP17@ITB301-COMP17>
2023-10-14 10:12:18 +0800

    login working now hehe

--- LoginForm.vb
+++ LoginForm.vb
@@ -1,5 +1,38 @@
 ﻿Public Class LoginForm
-    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    Private Sub LoginSubmit_Click(sender As Object, e As EventArgs) Handles LoginSubmit.Click
+        Dim uname, password As String
         Dim dbCon = New ConnectionDb()
+        Try
+            If LoginUsername.Text = String.Empty Then
+                Throw New Exception("Username field is required!")
+            End If
+
+            If LoginPassword.Text = String.Empty Then
+                Throw New Exception("Password field is required!")
+            End If
+
+            uname = LoginUsername.Text
+            password = LoginPassword.Text
+
+            Dim colList = New List(Of String)
+            colList.Add("Username")
+            colList.Add("Password")
+            colList.Add("Role")
+            Dim res = dbCon.getDataFrom("Users", colList, "Username", uname)
+            If res.Count = 0 Then
+                Throw New Exception("No user found!")
+            Else
+                If res.Item("Password").Equals(password) Then
+                    Console.WriteLine("TITE")
+                Else
+                    Throw New Exception("Incorrect password!")
+                End If
+            End If
+
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error Message", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 End Class
